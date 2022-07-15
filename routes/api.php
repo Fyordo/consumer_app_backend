66c257c6c85603797f8604977caef470e15825be
Fyordo <lagutinfedya@gmail.com>
2022-07-15 18:22:03 +0300

    Delete useless Controller

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware([\App\Http\Middleware\AuthUser::class])->group(function () {
-    include "api/chat.php";
     include "api/message.php";
 });
