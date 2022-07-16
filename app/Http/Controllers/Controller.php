c04f6fa0e25d016cb0713bc5e9c337e11775609b
Fyordo <lagutinfedya@gmail.com>
2022-07-16 18:54:35 +0300

    Add sensor imitation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\URL;
 
 /**
@@ -28,5 +30,11 @@ class Controller extends BaseController
                 'app' => env('APP_ENV')
             ]
         ];
+    }
+
+    public function checkSensor(){
+        Artisan::call('sensor:check');
+
+        return response()->json([]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sensor/check', [\App\Http\Controllers\Controller::class, "checkSensor"]);
