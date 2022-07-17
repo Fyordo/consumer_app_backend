a4f53bf2800e1de86128fad59a1b5e8f2d3ccdd7
Fyordo <lagutinfedya@gmail.com>
2022-07-17 09:24:42 +0300

    Add AI for graph calculation

--- app/Services/FlatManagerService.php
+++ app/Services/FlatManagerService.php
@@ -7,44 +7,25 @@ use App\Http\Resources\FlatResource;
 use App\Models\Client;
 use App\Models\ClientFlat;
 use App\Models\Flat;
+use Phpml\Regression\LeastSquares;
 
 class FlatManagerService
 {
     public function getGraph(Flat $flat) : array{
+        $samples = [[2021], [2022], [2023]];
+        $targets = [$flat->cost / 1.04, $flat->cost, $flat->cost * 1.15];
+
+        $regression = new LeastSquares();
+        $regression->train($samples, $targets);
+
         $result = [
             '2021' => $flat->cost / 1.04, // Цены с 2021 года повысились на 4%
             '2022' => $flat->cost,
             '2023' => $flat->cost * 1.15, // Эксперты предсказывают рост на 15%
         ];
 
-        $xValues = [-1, 0, 1];
-        $yValues = [$flat->cost / 1.04, $flat->cost, $flat->cost * 1.15];
-
-        for ($x = 2; $x < 11; $x++){
-            $size = count($result);
-
-            $lagrangePol = 0.0;
-
-            for ($i = 0; $i < $size; $i++){
-                $basicPol = 1.0;
-                for ($j = 0; $j < $size; $j++){
-                    if ($i !== $j){
-                        $basicPol *= ($x - $xValues[$j])/($xValues[$i] - $xValues[$j]);
-                    }
-                }
-
-                $lagrangePol += $basicPol * $yValues[$i];
-            }
-
-            $result[2022 + $x] = $lagrangePol;
-            $xValues[] = $x;
-            $yValues[] = $lagrangePol;
-        }
-
-        foreach ($result as $key => $value) {
-            if ($key % 2 != 0){
-                $result[$key] *= 0.6;
-            }
+        for ($i = 2024; $i <= 2032; $i++){
+            $result[(string)$i] = $regression->predict([$i]);
         }
 
         return $result;
